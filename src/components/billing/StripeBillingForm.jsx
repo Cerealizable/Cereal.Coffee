ef64851e1d95fc4da12edf40ca169f209bb4d442
Cerealizable <alberstc1@gmail.com>
2020-10-13 12:19:11 -0700

    fully functional stripe billing form created

--- src/components/billing/StripeBillingForm.jsx
+++ src/components/billing/StripeBillingForm.jsx
@@ -2,11 +2,20 @@ import React, { useState } from "react";
 import Button from "../customButtons/Button";
 import GridContainer from "../grid/GridContainer";
 import GridItem from "../grid/GridItem";
-import TextField from '@material-ui/core/TextField';
 
-import styles from '../../assets/jss/material-kit-react/views/loginPage';
+import styles from '../../assets/css/billingForm/billingFormStyle';
+import '../../assets/css/billingForm/billingform.css'
 import { makeStyles } from "@material-ui/core/styles";
 import { CardElement, injectStripe } from "react-stripe-elements";
+import Paper from '@material-ui/core/Paper';
+
+import Card from "../card/Card";
+import CardBody from "../card/CardBody";
+import CardFooter from "../card/CardFooter";
+import CustomInput from "../customInput/CustomInput";
+import Divider from "@material-ui/core/Divider";
+
+
 
 // drawer 
 // shopping cart
@@ -20,6 +29,12 @@ function StripeBillingForm({onSubmit, ...props}) {
   // CSS
   const useStyles = makeStyles(styles);
   const classes = useStyles();
+  const [cardAnimaton, setCardAnimation] = useState("cardHidden");
+
+  setTimeout(function() {
+    setCardAnimation("");
+  }, 700);
+
 
   const [customerName, setCustomerName] = useState("");
   const [price, setPrice] = useState(0);
@@ -38,7 +53,8 @@ function StripeBillingForm({onSubmit, ...props}) {
   }
 
   async function handleSubmitClick(e) {
-    e.prevenDefault();
+    console.log(" I am hitting my handleSubmitClick fx");
+    e.preventDefault();
 
     setIsProccessing(true);
 
@@ -51,56 +67,83 @@ function StripeBillingForm({onSubmit, ...props}) {
 
 
   return (
-    <div className={classes.container}>
-      <form onSubmit={handleSubmitClick}>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={4}>
-            <TextField
-              id="standard-textarea1"
-              label="Product Price"
-              multiline
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-            />
-          </GridItem>
-          <GridItem>
-            <TextField
-              id="standard-textarea2"
-              label="Product Quantity"
-              multiline
-              value={quantity}
-              onChange={(e) => setQuantity(e.target.value)}
-            />
-          </GridItem>
-          <GridItem>
-            <TextField
-              id="standard-textarea3"
-              label="Name on the Card"
-              multiline
-              value={customerName}
-              onChange={(e) => setCustomerName(e.target.value)}
-            />
-          </GridItem>
-          <GridItem>
-            <CardElement
-              className="card-field"
-              label="Credit Card Info"
-              style={{
-                base: { fontSize: "18px", fontFamily: '"Open Sans", sans-serif' }
-              }}
-              onChange={(e) => setIsCardComplete(e.complete)}
-            />
-          </GridItem>
-        </GridContainer>
-        <Button 
-          color="success"
-          type="submit"
-          disabled={!validateForm()}
-        >
-          Purchase
-        </Button>
-      </form>
-    </div>
+    <Paper>
+      <div className={classes.container}>
+        <GridContainer justify="center">
+            <GridItem xs={12} sm={12} md={10}>
+              <Card className={classes[cardAnimaton]}>
+                <form onSubmit={handleSubmitClick} className="BillingForm">
+                  <CardBody>
+                      <GridItem >
+                        <CustomInput
+                          labelText="Product Price"
+                          id="product-price"
+                          value={price}
+                          formControlProps={{
+                            fullWidth: true
+                          }}
+                          inputProps={{
+                            onChange: (e) => setPrice(e.target.value),
+                            autoFocus: true,
+                            type: "text",
+                          }}
+                        />
+                      </GridItem>
+                      <GridItem>
+                        <CustomInput
+                          labelText="Product Quantity"
+                          id="product-quantity"
+                          value={price}
+                          formControlProps={{
+                            fullWidth: true
+                          }}
+                          inputProps={{
+                            onChange: (e) => setQuantity(e.target.value),
+                            type: "text",
+                          }}
+                        />
+                      </GridItem>
+                      <Divider />
+                      <GridItem>
+                        <CustomInput
+                          labelText="Name"
+                          id="customer-name"
+                          value={customerName}
+                          formControlProps={{
+                            fullWidth: true
+                          }}
+                          inputProps={{
+                            onChange: (e) => setCustomerName(e.target.value),
+                            type: "text",
+                          }}
+                        />
+                      </GridItem>
+                      <GridItem>
+                        <CardElement
+                          className="card-field"
+                          label="Credit Card Info"
+                          style={{
+                            base: { fontSize: "18px", fontFamily: '"Open Sans", sans-serif' }
+                          }}
+                          onChange={(e) => setIsCardComplete(e.complete)}
+                        />
+                      </GridItem>
+                  </CardBody>
+                  <CardFooter className={classes.cardFooter}>
+                    <Button 
+                      color="success"
+                      type="submit"
+                      disabled={!validateForm()}
+                    >
+                      Purchase
+                    </Button>
+                  </CardFooter>
+                </form>
+              </Card>
+            </GridItem>
+          </GridContainer>
+      </div>
+    </Paper>
   )
 }
 
